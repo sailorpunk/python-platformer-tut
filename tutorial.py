176354856d176730e065f904db5198b1e072a5a5
sailorpunk <stevendezort@gmail.com>
2023-10-19 18:11:08 -0600

    started coding

---
+++ tutorial.py
@@ -0,0 +1,33 @@
+# Python game tutorial from https://www.youtube.com/watch?v=6gLeplbqtqg
+
+import os
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
+pygame.init()
+
+pygame.display.set_caption("Platformer")
+
+BG_COLOR = (255,255,255)
+WIDTH, HEIGHT = 1000, 800
+FPS = 60
+PLAYER_VEL = 5
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main(window):
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+if __name__ == "__main__":
+    main(window)
